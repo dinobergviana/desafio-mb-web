ad426376aba0e6ed1473fbc4c02e749b1fea2abe
Dinobergue Viana <dinbergueviana@MacBook-Air-de-Dinbergue.local>
2025-07-06 02:25:53 -0300

    feat: create StepFive component to display success status

---
+++ client/src/components/wizard/StepFive.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="success-page-container">
+    <h1 class="title">Cadastro realizado com sucesso!</h1>
+
+    <div class="actions">
+      <button class="btn-primary" type="button" @click="emitGoToFirstStep">Voltar para o início</button>
+    </div>
+  </section>
+</template>
+
+<script setup>
+  const props = defineProps({
+    formData: {
+      type: Object,
+      required: true
+    }
+  })
+
+  const emit = defineEmits(["next-step"])
+
+  function emitGoToFirstStep() {
+    emit("next-step", 0)
+  }
+</script>
+
+<style scoped>
+  .success-page-container {
+    margin-bottom: 0.25rem;
+  }
+
+  .success-page-container h1 {
+    margin-bottom: 1rem;
+  }
+</style>
